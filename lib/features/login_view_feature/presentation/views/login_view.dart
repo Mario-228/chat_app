e0facd64f65e113f827f26c99d18c1525f73378c
MarioYoussef <marioyoussef08@gmail.com>
2025-04-04 20:06:34 +0200

    refactor project structure files

---
+++ lib/features/login_view_feature/presentation/views/login_view.dart
@@ -0,0 +1,14 @@
+import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/widgets/login_view_body.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const LoginViewBody(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:customer_service_realtime_chat/home_view_feature/presentation/views/home_view.dart';
+import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/login_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeView(),
+      home: const LoginView(),
     );
   }
 }
