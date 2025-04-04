4591dde46904063226fbede253ec393f1a940c88
MarioYoussef <marioyoussef08@gmail.com>
2025-04-04 20:16:37 +0200

    create app colors class

---
+++ lib/core/util/app_colors/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color white = Colors.white;
+  static const Color lightGrey = Colors.grey;
+  static const Color black = Colors.black;
+}
