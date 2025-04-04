f82017059aaed59235e37b4543f1574508e8ee82
MarioYoussef <marioyoussef08@gmail.com>
2025-04-04 21:42:05 +0200

    create login view body

--- lib/features/login_view_feature/presentation/views/widgets/login_view_body.dart
+++ lib/features/login_view_feature/presentation/views/widgets/login_view_body.dart
@@ -1,3 +1,7 @@
+import 'package:customer_service_realtime_chat/core/util/app_fonts/app_fonts.dart';
+import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/widgets/login_button_and_forget_password_section.dart';
+import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/widgets/login_register_text_button_section.dart';
+import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/widgets/login_text_form_fields_section.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -5,10 +9,42 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('HomeViewBody'),
-      ],
+    GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Log in",
+                      style: AppFonts.textStyleBold36,
+                    ),
+                    SizedBox(height: 20.0),
+                    LoginTextFormFieldsSection(
+                        emailController: emailController,
+                        passwordController: passwordController),
+                    SizedBox(height: 40.0),
+                    LoginButtonAndForgetPasswordSection(formKey: formKey),
+                    SizedBox(height: 40.0),
+                    LoginRegisterTextButtonSection()
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
