1f0cdf5a0b79015a37c221cb5dbacc3ba9e1f108
MarioYoussef <marioyoussef08@gmail.com>
2025-04-04 21:37:55 +0200

    create custom material button

---
+++ lib/core/widgets/custom_material_button.dart
@@ -0,0 +1,30 @@
+import 'package:customer_service_realtime_chat/core/util/app_fonts/app_fonts.dart';
+import 'package:flutter/material.dart';
+
+class CustomMaterialButton extends StatelessWidget {
+  const CustomMaterialButton({
+    super.key,
+    required this.text,
+    required this.color,
+    this.onPressed,
+  });
+  final String text;
+  final Color color;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      minWidth: double.infinity,
+      onPressed: onPressed,
+      color: color,
+      height: 50.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(7.0),
+      ),
+      child: Text(
+        text,
+        style: AppFonts.textStyleBold19,
+      ),
+    );
+  }
+}
