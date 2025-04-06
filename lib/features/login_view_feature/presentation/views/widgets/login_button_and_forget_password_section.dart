708b7968b03fa0c19fee1b7acfc8d8444624cf74
MarioYoussef <marioyoussef08@gmail.com>
2025-04-06 22:50:20 +0200

    add the navigation to forget password

--- lib/features/login_view_feature/presentation/views/widgets/login_button_and_forget_password_section.dart
+++ lib/features/login_view_feature/presentation/views/widgets/login_button_and_forget_password_section.dart
@@ -1,6 +1,8 @@
 import 'package:customer_service_realtime_chat/core/util/app_fonts/app_fonts.dart';
+import 'package:customer_service_realtime_chat/core/util/app_router/app_router.dart';
 import 'package:customer_service_realtime_chat/core/widgets/custom_material_button.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginButtonAndForgetPasswordSection extends StatelessWidget {
   const LoginButtonAndForgetPasswordSection({
@@ -27,7 +29,8 @@ class LoginButtonAndForgetPasswordSection extends StatelessWidget {
         Expanded(
           child: TextButton(
             child: Text("Forgot Password", style: AppFonts.textStyleMostBold18),
-            onPressed: () {},
+            onPressed: () async =>
+                await GoRouter.of(context).push(AppRouter.kforgetPasswordView),
           ),
         ),
       ],
