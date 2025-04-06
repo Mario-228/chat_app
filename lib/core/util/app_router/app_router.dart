ad06cfe04db5126a0ec6427e67d2d0c771505af5
MarioYoussef <marioyoussef08@gmail.com>
2025-04-06 22:47:16 +0200

    add forget password view to routes

--- lib/core/util/app_router/app_router.dart
+++ lib/core/util/app_router/app_router.dart
@@ -1,12 +1,18 @@
+import 'package:customer_service_realtime_chat/features/forget_password_feature/presentation/views/forget_password_view.dart';
 import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/login_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+  static const String kforgetPasswordView = "/forgetPasswordView";
   static final routers = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: kforgetPasswordView,
+        builder: (context, state) => const ForgetPasswordView(),
       ),
     ],
   );
