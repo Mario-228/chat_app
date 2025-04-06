a37648098ba8a8d054e5e8cac25958b62887cd52
MarioYoussef <marioyoussef08@gmail.com>
2025-04-06 19:32:49 +0200

    add register view route

--- lib/core/util/app_router/app_router.dart
+++ lib/core/util/app_router/app_router.dart
@@ -1,12 +1,18 @@
 import 'package:customer_service_realtime_chat/features/login_view_feature/presentation/views/login_view.dart';
+import 'package:customer_service_realtime_chat/features/register_view_feature/presentation/views/register_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
+  static const kRegisterView = '/register-view';
   static final routers = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: kRegisterView,
+        builder: (context, state) => const RegisterView(),
       ),
     ],
   );
