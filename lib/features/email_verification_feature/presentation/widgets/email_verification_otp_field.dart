597fbd3f72cac9a5ba0e6d30de01ea219f231067
MarioYoussef <marioyoussef08@gmail.com>
2025-04-12 00:09:05 +0200

    build email verification view

---
+++ lib/features/email_verification_feature/presentation/widgets/email_verification_otp_field.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:customer_service_realtime_chat/core/util/app_fonts/app_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+
+class EmailVerificationOTPField extends StatelessWidget {
+  const EmailVerificationOTPField({super.key});
+  static String otp = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: OtpTextField(
+        textStyle: AppFonts.textStyleSemiBold20.copyWith(color: Colors.black),
+        numberOfFields: 6,
+        keyboardType: TextInputType.number,
+        filled: true,
+        showFieldAsBox: true,
+        onCodeChanged: (value) {
+          otp = value;
+        },
+        onSubmit: (String verificationCode) {
+          otp = verificationCode;
+          log(verificationCode);
+        },
+      ),
+    );
+  }
+}
