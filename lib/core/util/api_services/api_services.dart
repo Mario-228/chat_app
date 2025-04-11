59872584b5aa81ed5093fa2b682a40d261f6edac
MarioYoussef <marioyoussef08@gmail.com>
2025-04-11 19:58:13 +0200

    create api serivces class

---
+++ lib/core/util/api_services/api_services.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dioHelper;
+  ApiService(this.dioHelper);
+  Future<Map<String, dynamic>> get(String endPoint) async {
+    var response = await dioHelper.get(
+      endPoint,
+      options: Options(headers: {'ngrok-skip-browser-warning': 'true'}),
+    );
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>> postData(
+      String endPoint, Map<String, dynamic> query) async {
+    var response = await dioHelper.post(
+      endPoint,
+      queryParameters: query,
+      options: Options(headers: {'ngrok-skip-browser-warning': 'true'}),
+    );
+    return response.data;
+  }
+}
