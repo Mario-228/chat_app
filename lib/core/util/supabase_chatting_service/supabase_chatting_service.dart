b2a201e11eb9b71edacee2ab9c6351799cd7fcdb
MarioYoussef <marioyoussef08@gmail.com>
2025-04-15 01:10:32 +0200

    enable supabase streaming

--- lib/core/util/supabase_chatting_service/supabase_chatting_service.dart
+++ lib/core/util/supabase_chatting_service/supabase_chatting_service.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'package:customer_service_realtime_chat/core/util/supabase_chatting_service/supabase_constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -11,24 +10,7 @@ abstract class SupabaseChattingService {
     );
   }
 
-  static void createChannelChattingRoom(String tableName) {
-    RealtimeChannel channel =
-        supabase.channel("public:$tableName").onPostgresChanges(
-              event: PostgresChangeEvent.all,
-              schema: 'public',
-              table: tableName,
-              callback: (payload) {
-                log('🔥 Message received: ${payload.newRecord['Content']}');
-                log("old content =>${payload.oldRecord['Content']}");
-              },
-            )..subscribe();
-  }
-
-  static void getChannels(String tableName, Object primaryKey) {
-    supabase.from(tableName).stream(primaryKey: ['$primaryKey']).listen(
-      (List<Map<String, dynamic>> data) => log(
-        data.toString(),
-      ),
-    );
-  }
+  static SupabaseStreamFilterBuilder messagesStream(
+          String tableName, Object primaryKey) =>
+      supabase.from(tableName).stream(primaryKey: ['$primaryKey']);
 }
--- lib/features/chat_feature/presentation/views/widgets/chat_message_view.dart
+++ lib/features/chat_feature/presentation/views/widgets/chat_message_view.dart
@@ -3,20 +3,17 @@ import 'package:customer_service_realtime_chat/core/util/app_fonts/app_fonts.dar
 import 'package:flutter/material.dart';
 
 class ChatMessageView extends StatelessWidget {
-  const ChatMessageView({super.key, required this.index});
-  final int index;
-  //the index is for displaying the message from left or right
-
+  const ChatMessageView({super.key, required this.text});
+  final String text;
   @override
   Widget build(BuildContext context) {
-    bool isSender = index % 2 == 0;
     return BubbleSpecialThree(
-      text: "PlaceHolderData $index",
+      text: text,
       tail: true,
-      isSender: isSender,
-      color: isSender
-          ? Color.fromARGB(100, 63, 135, 255)
-          : Color.fromARGB(100, 246, 246, 246),
+      isSender: false,
+      color: Color.fromARGB(100, 246, 246, 246),
+      // ? Color.fromARGB(100, 63, 135, 255)
+      // : ,
       textStyle: AppFonts.textStyleBold19,
     );
   }
