0bbcf9460ddfcb02cd05aef89622d486cbfab87c
TDH9001 <mohamedyehia707@gmail.com>
2025-04-14 21:11:01 +0200

    added a router button to Chat screen, found in actions

--- lib/features/chat_feature/presentation/views/chat_view.dart
+++ lib/features/chat_feature/presentation/views/chat_view.dart
@@ -1,5 +1,6 @@
 import 'package:customer_service_realtime_chat/core/widgets/custom_appbar.dart';
 import 'package:customer_service_realtime_chat/features/chat_feature/presentation/views/widgets/chat_view_body.dart';
+import 'package:customer_service_realtime_chat/features/chat_feature/presentation/views/widgets/ticket_router_action_button.dart';
 import 'package:flutter/material.dart';
 
 class ChatView extends StatelessWidget {
@@ -8,7 +9,12 @@ class ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(title: "Chat ",),
+      appBar: CustomAppbar(
+        title: "Chat ",
+        actions: [
+          TicketRouterActionButton(),
+        ],
+      ),
       body: ChatViewBody(),
     );
   }
