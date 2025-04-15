1758e435a515d2d12047f183daccd5dc3964a1d6
TDH9001 <mohamedyehia707@gmail.com>
2025-04-15 02:16:53 +0200

    post merge minor edit

--- lib/features/submit_ticket_feature/data/repo/submit_ticket_repo_execution.dart
+++ lib/features/submit_ticket_feature/data/repo/submit_ticket_repo_execution.dart
@@ -1,6 +1,7 @@
 import 'package:customer_service_realtime_chat/core/errors/errors.dart';
 import 'package:customer_service_realtime_chat/core/util/api_services/api_services.dart';
 import 'package:customer_service_realtime_chat/core/util/api_services/base_url.dart';
+import 'package:customer_service_realtime_chat/core/util/cache_helper/cache_helper.dart';
 import 'package:customer_service_realtime_chat/features/submit_ticket_feature/data/model/tocket_form_model.dart';
 import 'package:customer_service_realtime_chat/features/submit_ticket_feature/data/repo/submit_ticket_repo.dart';
 import 'package:customer_service_realtime_chat/features/submit_ticket_feature/data/repo/submit_ticket_repo_endpoint.dart';
@@ -12,8 +13,10 @@ class SubmitTicketRepoExecution extends SubmitTicketRepo {
   Future<Either<Errors, void>> submitTicket(
       {required TicketFormModel tocketFormModel}) async {
     try {
-      await ApiService(BaseUrl.api).postData(
-          SubmitTicketRepoEndpoint.endPoint, tocketFormModel.toJson());
+      await ApiService(BaseUrl.api).postWithToken(
+          SubmitTicketRepoEndpoint.endPoint,
+          tocketFormModel.toJson(),
+          CacheHelper.getLoginData().token);
       return right(null);
     } on Exception catch (e) {
       if (e is DioException) {
