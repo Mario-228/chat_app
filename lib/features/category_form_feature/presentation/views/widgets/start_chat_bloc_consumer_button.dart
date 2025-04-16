2926057b99641eb8747f42212942ae430acf94c7
TDH9001 <mohamedyehia707@gmail.com>
2025-04-16 09:19:52 +0200

    finished the app, completly this time realy

--- lib/features/category_form_feature/presentation/views/widgets/start_chat_bloc_consumer_button.dart
+++ lib/features/category_form_feature/presentation/views/widgets/start_chat_bloc_consumer_button.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'package:customer_service_realtime_chat/core/util/cache_helper/cache_helper.dart';
 import 'package:customer_service_realtime_chat/core/widgets/custom_loading_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,26 +16,19 @@ class StartChatBlocConsumerButton extends StatelessWidget {
   });
 
   @override
-/*************  ✨ Windsurf Command ⭐  *************/
-  /// A widget that listens to the [CreateChatRoomCubit] and handles all the cases of the [CreateChatRoomStates].
-  ///
-  /// If the state is [CreateChatRoomError], it will show a snackbar with the error message.
-  ///
-  /// If the state is [CreateChatRoomSuccess], it will push replacement to [AppRouter.kchatView] with the chat id as extra.
-  ///
-  /// If the state is [CreateChatRoomLoading], it will display a [CustomLoadingWidget].
-  ///
-  /// If the state is not one of the above, it will display a [CustomMaterialButton] with the text "start chat".
-  /// When the button is pressed, it will call [CreateChatRoomCubit.createChatRoom] and return the result.
-/*******  e65cd959-c114-48f2-9a79-b593079df490  *******/  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return BlocConsumer<CreateChatRoomCubit, CreateChatRoomStates>(
       listener: (context, state) {
         if (state is CreateChatRoomError) {
           showSnackBar(context, message: state.error);
         } else if (state is CreateChatRoomSuccess) {
           log(state.chatId.toString());
-          GoRouter.of(context)
-              .pushReplacement(AppRouter.kchatView, extra: state.chatId);
+          if (CacheHelper.getLoginData().role == "Customer") {
+            GoRouter.of(context).push(AppRouter.kchatView, extra: state.chatId);
+          } else {
+            GoRouter.of(context)
+                .pushReplacement(AppRouter.kchatView, extra: state.chatId);
+          }
         }
       },
       builder: (context, state) {
@@ -42,7 +36,9 @@ class StartChatBlocConsumerButton extends StatelessWidget {
           return const CustomLoadingWidget();
         } else {
           return CustomMaterialButton(
-            text: "start chat",
+            text: CacheHelper.getLoginData().role == "Customer"
+                ? "start chat"
+                : "support a customer",
             onPressed: () async {
               await CreateChatRoomCubit.get(context).createChatRoom();
             },
